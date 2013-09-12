93d6a734ba2779ad8a73bb648ed21cee95bd9311
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2013-09-12 12:47:13 +0200

    sap

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,13 +3,15 @@ define([
   'controllers/MainController',
   'handlebars',
   'router',
-
   'models/user'
   ], function (Marionette, MainController, Handlebars, AppRouter, User) {
 
+    console.log("YEAH SAP1");
     Backbone.Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
+      console.log("YEAH SAP1");
       return Handlebars.compile(rawTemplate);
     };
+    console.log("YEAH SAP2");
 
     // set up the app instance
     window.Shredhub = new Marionette.Application();
@@ -23,7 +25,7 @@ define([
    });
 
     Shredhub.addInitializer(function(options){
-     
+      console.log("YEAH SAP13");
       Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
         if (arguments.length <3)
           throw new Error("Handlebars Helper equal needs 2 parameters");
@@ -36,6 +38,7 @@ define([
     });
 
     Shredhub.addInitializer(function(options) {
+      console.log("YEAH SAP112");
       $.ajaxSetup({
         beforeSend: function(xhr) {
           xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
@@ -44,17 +47,20 @@ define([
     });
 
     Shredhub.addInitializer(function(options){
+      console.log("YEAH SAP112q");
       window.router = new AppRouter({controller:options.controller});
       Backbone.history.start();
     });
 
     Shredhub.addInitializer (function(options) {
+      console.log("YEAH SAP1dasda");
       if ( window.user) {
         Shredhub.user = new User(window.user);
         Shredhub.user.initUser(window.user);
       }
     });
     
+    console.log("YEAH SAP1asdasdasd");
     window.mainController = new MainController();
     Shredhub.start({controller : mainController} );
 
